40dbe51242cccc1c99963c0954f72a498de237c7
AlvinChrist <alvin.ardiansyah@pt-ssss.com>
2022-12-09 17:06:55 +0700

    update day 8

--- 2022/8/b.js
+++ 2022/8/b.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 try {
-    const queries = fs.readFileSync('a.txt', 'utf8').split('\r\n');
-    const vis = []
+    const queries = fs.readFileSync('b.txt', 'utf8').split('\r\n');
+    const vis = [];
+    const scores = [];
     const grid = queries.map((lines) => lines.split('').map((x) => +x))
     const n = grid.length;
     const m = grid[0].length;
 
     let count = 0;
+    vis[0] = Array(m).fill(0);
+    vis[n-1] = Array(m).fill(0);
+    
     for(let i = 1; i < n-1; i++) {
-        vis[i] = []
+        vis[i] = Array(m).fill(0)
         const cols = grid[i]
         const left = cols[0];
         const right = cols[m - 1];
@@ -25,7 +29,7 @@ try {
                 let valid = true;
                 for(let k = 1; k < j; k++) {
                     if(cols[k] >= cols[j]) {
-                        valid = false;
+                        valid = 0;
                         break;
                     }
                 }
@@ -35,15 +39,15 @@ try {
                     if(!vis[i]) {
                         vis[i] = []
                     }
-                    vis[i][j] = true;
+                    vis[i][j] = 1;
                 }
             }
             if(vis[i][j] || vis[i][j] === -1) continue;
             if(right < cols[j]) {
-                let valid = true;
+                let valid = 1;
                 for(let k = j + 1; k < m - 1; k++) {
                     if(cols[k] >= cols[j]) {
-                        valid = false;
+                        valid = 0;
                         break;
                     }
                 }
@@ -53,7 +57,7 @@ try {
                     if(!vis[i]) {
                         vis[i] = []
                     }
-                    vis[i][j] = true;
+                    vis[i][j] = 1;
                 }
             }
         }
@@ -69,7 +73,7 @@ try {
                 let valid = true;
                 for(let k = 1; k < j; k++) {
                     if(grid[k][i] >= grid[j][i]) {
-                        valid = false;
+                        valid = 0;
                         break;
                     }
                 }
@@ -79,7 +83,7 @@ try {
                     if(!vis[j]) {
                         vis[j] = []
                     }
-                    vis[j][i] = true;
+                    vis[j][i] = 1;
                 }
             }
             if(vis[j][i] || vis[j][i] === -1) continue;
@@ -87,7 +91,7 @@ try {
                 let valid = true;
                 for(let k = j + 1; k < n - 1; k++) {
                     if(grid[k][i] >= grid[j][i]) {
-                        valid = false;
+                        valid = 0;
                         break;
                     }
                 }
@@ -97,12 +101,64 @@ try {
                     if(!vis[j]) {
                         vis[j] = []
                     }
-                    vis[j][i] = true;
+                    vis[j][i] = 1;
                 }
             }
         }
     }
-    console.log(count + 2 * (m + n) - 4)
+    
+    let scenic = 0;
+    
+    for(let i = 1; i < n-1; i++) {
+        for(let j = 1; j < m-1; j++) {
+            let score = 1;
+            if(vis[i][j] === 1) {
+                // check left
+                let k = 0
+                let count = 0
+                for(k = j-1; k >= 0; k--) {
+                    count++;
+                    if(grid[i][k] >= grid[i][j]) {
+                        break;
+                    }
+                }
+                score *= count;
+                // check right
+                count = 0;
+                for(k = j + 1; k < m; k++) {
+                    count++;
+                    if(grid[i][k] >= grid[i][j]) {
+                        break;
+                    }
+                }
+                score *= count;
+                // check top
+                count = 0;
+                for(k = i-1; k >= 0; k--) {
+                    count++;
+                    if(grid[k][j] >= grid[i][j]) {
+                        break;
+                    }
+                }
+                score *= count;
+                // check bottom
+                count = 0
+                for(k = i + 1; k < n; k++) {
+                    count++;
+                    if(grid[k][j] >= grid[i][j]) {
+                        break;
+                    }
+                }
+                score *= count;
+            }
+            vis[i][j] = score;
+            if(score > scenic) scenic = score;
+        }
+    }
+    console.log(scenic)
+    // for(let row of vis) {
+    //     console.log(row)
+    // }
     
 } catch (e) {
     console.log(e)
